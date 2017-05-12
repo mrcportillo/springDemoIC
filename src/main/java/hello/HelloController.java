1c34015e9b3e4b03a23acb10a40c2b579df1610f
mrcportillo <marcossportillo@gmail.com>
2017-05-12 10:51:54 -0300

    Modificado controller, agregado endpoint

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalTime;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -19,5 +20,14 @@ public class HelloController {
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @RequestMapping("/timeGreeting")
+    public Greeting landingTime() {
+        if (LocalTime.now().isAfter(LocalTime.of(12,0))) {
+            return new Greeting(counter.incrementAndGet(), String.format(template, "night"));
+        } else {
+            return new Greeting(counter.incrementAndGet(), String.format(template, "morning"));
+        }
     }
 }
