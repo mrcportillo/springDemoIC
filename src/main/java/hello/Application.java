d4b5faafd3a3c3c1f250f388234f04956776b779
mrcportillo <marcossportillo@gmail.com>
2017-05-18 15:50:49 -0300

    activado app

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,15 +1,12 @@
-/*
 package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-*/
 /**
  * Created by mrc on 15/04/17.
- *//*
-
+ */
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
@@ -18,4 +15,3 @@ public class Application extends SpringBootServletInitializer{
         SpringApplication.run(Application.class, args);
     }
 }
-*/
